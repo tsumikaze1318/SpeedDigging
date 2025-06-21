f5d6e95074caa89c6a3623631cd70263ce717539
Nanashisann <mikagetotemokawaii12@gmail.com>
2025-06-21 18:01:17 +0900

    timer

--- Assets/Menber/Matsumoto/TimeUI.cs
+++ Assets/Menber/Matsumoto/TimeUI.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeUI : MonoBehaviour
 {
+    [SerializeField]
+
+    TextMeshProUGUI TimerText;
+
+    float limitTime = 60;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,13 @@ public class TimeUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        limitTime -= Time.deltaTime;
+
+        if (limitTime < 0 )
+        {
+            limitTime = 0;
+        }
+
+        TimerText.text = limitTime.ToString("F0");
     }
 }
